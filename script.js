a74a41c88b2f346bdef485f15a85531fd45f01ed
Shradha Agarwal <62115289+shradhaagarwal01@users.noreply.github.com>
2020-07-24 16:29:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  checkGuess(humanGuess);
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
+
+function checkGuess(guess){
+  if (guess<0 || guess>9){
+    window.alert('Make a guess between 0 and 9');
+  }
+}
